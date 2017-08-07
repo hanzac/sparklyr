b11199aeabad628cfca2cd7402cc2af5245d150a
Javier Luraschi <javierluraschi@hotmail.com>
2017-08-06 21:45:07 -0700

    add support for yarn-cluster in high-availability clusters

--- R/yarn_cluster.R
+++ R/yarn_cluster.R
@@ -16,10 +16,29 @@ spark_yarn_cluster_get_gateway <- function() {
 
   yarnSiteXml <- xml2::read_xml(yarnSite)
 
+  yarnResourceManagerHighAvailabilityId <- xml2::xml_text(xml2::xml_find_all(
+    yarnSiteXml,
+    "//name[.='yarn.resourcemanager.ha.id']/parent::property/value")
+  )
+
+  mainResourceManager <- "yarn.resourcemanager.address"
+  if (length(yarnResourceManagerHighAvailabilityId) > 0) {
+    mainResourceManager <- paste(
+      "yarn.resourcemanager.address.",
+      yarnResourceManagerHighAvailabilityId,
+      sep = ""
+    )
+  }
+
   yarnResourceManagerAddress <- xml2::xml_text(xml2::xml_find_all(
     yarnSiteXml,
-    "//name[.='yarn.resourcemanager.address']/parent::property/value")
-  )
+    paste(
+      "//name[.='",
+      mainResourceManager,
+      "']/parent::property/value",
+      sep = ""
+    )
+  ))
 
   if (length(yarnResourceManagerAddress) == 0) {
     stop("Yarn Cluster mode uses `yarn.resourcemanager.address` but is not present in yarn-site.xml")
